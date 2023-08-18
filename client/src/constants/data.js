29af6714577055f50caf7a3ce6e31621efe6add6
AyushGupta0218 <102624011+AyushGupta0218@users.noreply.github.com>
2023-08-18 11:14:58 +0530

    Add files via upload

---
+++ client/src/constants/data.js
@@ -0,0 +1,7 @@
+export const categories = [
+    { id: 1, type: "Fest/events" },
+    { id: 2, type: "Academics" },
+    { id: 3, type: "Lost & Found" },
+    { id: 4, type: "Hostel queries" },
+    { id: 5, type: "Entertainment" }
+];
